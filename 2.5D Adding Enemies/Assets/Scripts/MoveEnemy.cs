6b38768c264873cf87255f2311b77dc943230984
gradyShepherd <gradyshepherd13@gmail.com>
2017-05-12 18:42:50 -0600

    Enemies and Player

---
+++ 2.5D Adding Enemies/Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveEnemy : MonoBehaviour
+{
+    public float AGGRESSIVE_RANGE = 15.0f;
+
+    float distToPlayer;
+    Transform player;               // Reference to the player's position.
+    Transform tree;
+    //playerHealth;                   // Reference to the player's health.
+    //EnemyHealth enemyHealth;        // Reference to this enemy's health.
+    NavMeshAgent nav;               // Reference to the nav mesh agent.
+
+
+    void Awake()
+    {
+        // Set up the references.
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        tree = GameObject.FindGameObjectWithTag("Tree").transform;
+        //playerHealth = player.GetComponent<PlayerHealth>();
+        //enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        distToPlayer = Vector3.Distance(player.position, transform.position);
+        //Debug.Log(distToPlayer);
+        //ENEMY MOVES TO ATTACK PLAYER IF WITHIN AGGRESSIVE RANGE
+        if (distToPlayer >= AGGRESSIVE_RANGE)
+        {
+            nav.SetDestination(tree.position);
+        }
+        else
+        {
+            nav.SetDestination(player.position);
+        }
+    }
+}
---
+++ 2.5D Adding Enemies/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed = 20f;           
+
+    Vector3 movement;                   
+    Animator anim;                      
+    Rigidbody playerRigidbody;         
+    int floorMask;                      
+    //float camRayLength = 100f;          
+
+    void Awake()
+    {
+     
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Animating(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Animating(float h, float v)
+    {
+        bool isWalkingVert;
+        bool isWalkingHoriz;
+
+        if (!Input.GetButtonDown("Attack Down") && !Input.GetButtonDown("Attack Up") && 
+            !Input.GetButtonDown("Attack Left") && !Input.GetButtonDown("Attack Right"))
+        {
+            if (v != 0)
+            {
+                isWalkingVert = true;
+                isWalkingHoriz = false;
+            }
+            else if (h != 0)
+            {
+                isWalkingHoriz = true;
+                isWalkingVert = false;
+            }
+            else
+            {
+                isWalkingVert = false;
+                isWalkingHoriz = false;
+            }
+        }
+    }
+}
